ff0e7afe1bd8af24a30282951ac63cccedddcde5
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-15 16:19:38 -0400

    Allowing support for longer text values

--- script/create_form.rb
+++ script/create_form.rb
@@ -18,7 +18,7 @@ number;data_type;prompt;required
 10;text;Cell phone(s);false
 11;explanation;Although a passenger cell phone is not required, it is the best way for us to ensure that we can quickly and easily connect with our passenger(s). Please consider providing one.;false
 12;heading;Trip Information;false
-14;explanation;Vehicle requests are subject to availability.;false
+14;explanation;All vehicle requests are subject to availability.;false
 15;date/time;Pickup date and time;true
 16;text;Pickup location;true
 17;text;Destination;true
@@ -27,11 +27,11 @@ number;data_type;prompt;required
 20;text;Airline or provider;false
 21;time;Arrival/departure time;false
 22;heading;Notes;false
-23;text;Notes and special requests;false
+23;long-text;Notes and special requests;false
 FIELDS
     attrs = row.to_hash.merge form_id: form.id
     Field.create! attrs
   end
   # Couldn't make options work with CSV, so create manually
-  Field.create! form_id: form.id, number: 13, data_type: 'options', prompt: 'Vehicle requested', required: true, options: ['Sedan', 'Van']
+  Field.create! form_id: form.id, number: 13, data_type: 'options', prompt: 'Desired vehicle type', required: true, options: ['Sedan', 'Van']
 end
