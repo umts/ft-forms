10ef69deb29cd969760c06c15d78a80c972e91c5
Karin Eichelman <keichelman@gmail.com>
2015-06-15 12:57:07 -0400

    wrote tests for form controller

--- spec/controllers/forms_controller_spec.rb
+++ spec/controllers/forms_controller_spec.rb
@@ -1,4 +1,20 @@
 require 'rails_helper'
 
 describe FormsController do
+  describe 'GET #show' do
+    before :each do
+      @form = create :form
+    end
+    let :submit do
+      get :show, id: @form.id
+    end
+    it 'assigns the correct instance variable' do
+      submit
+      expect(assigns.fetch :form).to eql @form
+    end
+    it 'displays the correct template' do
+      submit
+      expect(response).to render_template :show
+    end
+  end
 end
---
+++ spec/factories/forms.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :form do
+    name 'form name'
+  end
+end
+    
