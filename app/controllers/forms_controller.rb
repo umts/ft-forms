2fe0c058ee513c1471fc4e2a51adce16ac054b37
David Faulkenberry <david.faulkenberry@gmail.com>
2015-06-22 16:32:40 -0400

    Fixing overly long line with the concatentation which Rubocop prefers

--- app/controllers/forms_controller.rb
+++ app/controllers/forms_controller.rb
@@ -24,7 +24,8 @@ class FormsController < ApplicationController
     # form_changes = params.require(:form).permit :name
     @form.assign_attributes @form_changes
     @preview = true
-    flash[:message] = 'You are previewing your changes. This form, as shown here, is not live.'
+    flash[:message] = 'This is a preview of your changes. ' \
+                      'This form, as shown here, is not live.'
     render 'show'
   end
 
