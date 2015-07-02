f851b752479949bcabdcd0f6c156e04fd6418e33
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-02 14:59:36 -0400

    Probably not worth disabling this app-wide
    But it's starting to get old

--- app/controllers/fields_controller.rb
+++ app/controllers/fields_controller.rb
@@ -1,9 +1,12 @@
 class FieldsController < ApplicationController
   before_action :find_field
 
+  # '...and return' is correct here, disable rubocop warning
+  # rubocop:disable Style/AndOr
   def edit
     redirect_to :back and return unless @field.form_draft.present?
   end
+  # rubocop:enable Style/AndOr
 
   def update
     @field.update! options: params.require(:options).split("\r\n")
