712b5af299c0092bc9376c8cfe5c4b3e736f76df
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-13 14:04:09 -0400

    Production user

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,7 +3,7 @@
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
 
-# server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
+server 'af-transit-app3.admin.umass.edu', roles: %w{app db web}, user: (Net::SSH::Config.for('af-transit-app3.admin.umass.edu')[:user] || ENV['USER'])
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
 
