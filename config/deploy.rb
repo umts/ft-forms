83cdf42a78b1177825b89f704e3206ded81dace5
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-13 13:58:14 -0400

    Don't include database.yml, begin setting up Capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,14 @@
 # config valid only for current version of Capistrano
 lock '3.4.0'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :application, 'ft-forms'
+set :repo_url, 'git@github.com:umts/ft-forms.git'
 # set :passenger_restart_with_touch, true
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/ft-forms'
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -32,8 +31,7 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
