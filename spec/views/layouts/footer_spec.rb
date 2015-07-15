5b0bd1fd8d8b4914f3bdf7b272dd759a56bd7864
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-15 15:09:16 -0400

    Footer specs; #15
    Because we live in a digital world.

--- spec/views/layouts/footer_spec.rb
+++ spec/views/layouts/footer_spec.rb
@@ -3,10 +3,30 @@ include RSpecHtmlMatchers
 
 describe 'layouts/_footer.haml' do
   # From the UMass Digital Brand Guide - "We live in a digital world."
-  it 'has a span with a class of content'
-  it 'includes the copyright year'
-  it 'includes a link to umass.edu'
-  it 'links to the correct Site Policies URL'
-  it 'has us as the Site Contact Email'
-  it 'is bullet-separated'
+  it 'has a span with a class of content' do
+    render
+    expect(rendered).to have_tag 'span.content'
+  end
+  it 'includes the copyright year' do
+    render
+    expect(rendered).to include Time.now.year.to_s
+  end
+  it 'includes a link to umass.edu' do
+    render
+    expect(rendered).to have_tag 'a', with: { href: 'http://umass.edu' }
+  end
+  it 'links to the correct Site Policies URL' do
+    render
+    expect(rendered)
+      .to have_tag 'a', with: { href: 'http://umass.edu/site_policies' } do
+      with_text 'Site Policies'
+    end
+  end
+  it 'has us as the Site Contact Email' do
+    render
+    expect(rendered)
+      .to have_tag 'a', with: { href: 'transit-it@admin.umass.edu' } do
+      with_text 'Site Contact'
+    end
+  end
 end
