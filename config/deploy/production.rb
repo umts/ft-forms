d96b26b0afbc5dac4db0d5e1cabf1603455dea3b
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-15 16:02:12 -0400

    Undo that

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,4 +2,3 @@ server 'af-transit-app3.admin.umass.edu',
         roles: %w{app db web},
         user: (Net::SSH::Config.for('af-transit-app3.admin.umass.edu')[:user] || ENV['USER'])
 set :bundle_env_variables, { 'NOKOGIRI_USE_SYSTEM_LIBRARIES' => 1 }
-set :passenger_environment_variables, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/var/run/passenger-instrg/ft-forms' }
