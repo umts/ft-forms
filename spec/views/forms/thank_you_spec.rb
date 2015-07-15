e8242af47f60c3fb7fe333edb0ab56db460ec129
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-15 13:35:24 -0400

    Testing forms/thank_you; #15

---
+++ spec/views/forms/thank_you_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+include RSpecHtmlMatchers
+
+describe 'forms/thank_you.haml' do
+  it 'has an h1 tag saying thanks' do
+    render
+    expect(rendered).to have_tag 'h1' do
+      with_text 'Thank you!'
+    end
+  end
+  it 'has some other text' do
+    render
+    expect(rendered).to include 'Your request has been processed'
+    expect(rendered).to include 'and you should receive'
+    expect(rendered).to include 'a confirmation email shortly.'
+  end
+end
