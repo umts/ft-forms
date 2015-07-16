f37d9f6eaa9da02e4aaa39ce405e12fd0376f2c4
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-16 12:45:30 -0400

    Temporarily, some logging

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,8 +38,12 @@ class ApplicationController < ActionController::Base
   def set_spire
     if spire_exists?
       session[:spire] ||= request.env['SPIRE_ID']
-    else redirect_to unauthenticated_session_path and return
-      # something has gone terribly, awfully wrong
+    else # something has gone terribly, awfully wrong
+      logger.debug 'Request:'
+      logger.debug request.inspect
+      logger.debug 'Session:'
+      logger.debug session.inspect
+      redirect_to unauthenticated_session_path and return
     end
   end
   # rubocop:enable Style/AndOr
