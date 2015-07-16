125580f03a315b7b3e30703da4e5d0b8547d5960
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-16 14:39:31 -0400

    Strip secrets from logs and exception emails

--- config/application.rb
+++ config/application.rb
@@ -6,12 +6,10 @@ Bundler.require(*Rails.groups)
 
 module FtForms
   class Application < Rails::Application
-    config.generators do |g|
-      g.assets          false
-      g.helpers         false
-      g.stylesheets     false
-      g.template_engine :haml
-      g.test_framework  :rspec
-    end
+    config.encoding = 'utf-8'
+    config.time_zone = 'Eastern Time (US & Canada)'
+    config.autoload_paths << Rails.root.join('lib')
+    config.autoload_paths << Rails.root.join('vendor/lib')
+    config.filter_parameters += [:password, :secret, :spire, :github]
   end
 end
