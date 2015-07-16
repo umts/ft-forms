4365b458425cc42a0a27fd62ce419985ef4102e2
David Faulkenberry <david.faulkenberry@gmail.com>
2015-07-16 13:44:37 -0400

    Mail logging

--- app/mailers/ft_forms_mailer.rb
+++ app/mailers/ft_forms_mailer.rb
@@ -5,11 +5,13 @@ class FtFormsMailer < ActionMailer::Base
     @form_data = form_data
     mail to: form_data.fetch('Email'),
          subject: 'Meet & Greet Request Confirmation'
+    logger.info "Sent mail to #{form_data.fetch 'Email'}"
   end
 
   def send_form(form_data)
     @form_data = form_data
     mail to: 'fieldtrip@umass.edu',
          subject: 'New Meet & Greet Request'
+    logger.info "Sent mail to fieldtrip@umass.edu"
   end
 end
