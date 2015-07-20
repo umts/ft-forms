ce93178a37c3a4e295463fc9e7c4bec8e0385f19
Karin Eichelman <keichelman@gmail.com>
2015-07-20 11:43:49 -0400

    mailer view tests

---
+++ spec/views/ft_forms_mailer/send_confirmation_text_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+include RSpecHtmlMatchers
+
+describe 'ft_forms_mailer/send_confirmation.text.erb' do
+  before :each do
+    @form_data = Hash[ question1: 'answer1', question2: 'answer2', email: 'my_email']
+  end
+  it 'includes the text of the email' do
+    render
+    expect(rendered).to include 'UMass Transit Meet & Greet Service'
+  end
+  it 'includes the form data of the request' do
+    render
+    expect(rendered).to include 'question1: answer1',
+                                'question2: answer2',
+                                'email: my_email'
+  end
+end
