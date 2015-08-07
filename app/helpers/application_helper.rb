4c92c2c7dfac5672ed8d00b41a268b3fa3ff5bf3
David Faulkenberry <david.faulkenberry@gmail.com>
2015-08-07 10:44:39 -0400

    Update gems also

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,10 +3,10 @@ module ApplicationHelper
   # based on the data type of the field.
   def input_class(data_type)
     case data_type
-    when 'date'      then 'datepicker'
+    when 'date' then 'datepicker'
     when 'date/time' then 'datetimepicker'
-    when 'text'      then  nil
-    when 'time'      then 'timepicker'
+    when 'text' then nil
+    when 'time' then 'timepicker'
     end
   end
 
