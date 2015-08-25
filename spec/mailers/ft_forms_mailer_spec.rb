eb4b4fb8596145c8807b7e07f21157587353b19c
Karin Eichelman <keichelman@gmail.com>
2015-08-25 12:12:00 -0400

    wrote tests to add comma separated email addresses.
    closes #35

--- spec/mailers/ft_forms_mailer_spec.rb
+++ spec/mailers/ft_forms_mailer_spec.rb
@@ -41,6 +41,12 @@ describe FtFormsMailer do
     it 'sends to the email of the form' do
       expect(output.to).to eql Array(@form.email)
     end
+    it 'sends to multiple emails of the form if separated by comma' do
+      email_1 = 'person@test.host'
+      email_2 = 'people@test.host'
+      @form.update email: [email_1, email_2].join(', ')
+      expect(output.to).to eql [email_1, email_2]
+    end
     it 'has a subject of New Meet & Greet Request' do
       expect(output.subject).to eql 'New Meet & Greet Request'
     end
