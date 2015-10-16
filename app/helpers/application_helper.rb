586b9d158dec712f17ecc79c189c6c3df7a90710
Karin Eichelman <keichelman@gmail.com>
2015-10-16 16:19:07 -0400

    this problem is not solvable this way

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,7 @@ module ApplicationHelper
     responses = []
     data.each do |k, v|
       data_type, number = k.split '_'
+      binding.pry
       case data_type
       when 'field'
         responses[number.to_i] = v
