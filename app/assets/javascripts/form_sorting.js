c75b818c9a74d6b9c26d5356264e22780a14723a
anbranin <keichelman@gmail.com>
2017-06-18 00:01:59 -0400

    form sorts, but disables selection when cancel is used.
    going to try something else to add new fields.

--- app/assets/javascripts/form_sorting.js
+++ app/assets/javascripts/form_sorting.js
@@ -2,3 +2,23 @@ $( document ).ready( function() {
   $('.sortable').sortable({
   });
 });
+
+
+
+function reNumber() {
+  $('.grabbable');
+}
+
+function serializeFields() {
+
+}
+
+//when new question is added:
+//$( ".selector" ).sortable( "refresh" );
+//reload all sortable items, cause new to be recognized
+//
+//send new order to server: http://api.jqueryui.com/sortable/#method-serialize
+//$( ".sortable" ).sortable( "serialize", { key: "sort", expression: /(\d)[_](.+)/ } );
+//
+//span class?
+//ui-icon ui-icon-arrowthick-2-n-s
