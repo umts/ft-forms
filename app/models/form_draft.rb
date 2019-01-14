1245506c5818dfa27aca326303cff03ae63756b7
Anbranin <keichelman@gmail.com>
2019-01-14 16:46:13 -0500

    make update_form handle the fact that there may not have been a form to start with

--- app/models/form_draft.rb
+++ app/models/form_draft.rb
@@ -14,7 +14,12 @@ class FormDraft < ApplicationRecord
   end
 
   def update_form!
-    form.update(attributes.except 'form_id', 'user_id', 'id')
+    form_attributes = attributes.except 'form_id', 'user_id', 'id'
+    if form.present?
+      form.update form_attributes
+    else
+      form.create form_attributes
+    end
     # Don't need to retain the fields, since the draft will be deleted.
     # Just switch them over to belonging to the form.
     form.fields.delete_all
