77881b4d5040f9d21949bc415a57b276f687f386
Anbranin <keichelman@gmail.com>
2019-02-01 17:25:30 -0500

    line length

--- app/models/field.rb
+++ app/models/field.rb
@@ -22,7 +22,8 @@ class Field < ApplicationRecord
                                    if: -> { form_draft.present? } }
   validates :required,
             inclusion: { in: [true, false],
-                         message: 'must be true or false', unless: -> { explanation? || heading? } }
+                         message: 'must be true or false',
+                         unless: -> { explanation? || heading? } }
 
   default_scope { order :number }
   scope :not_new, -> { where.not id: nil }
