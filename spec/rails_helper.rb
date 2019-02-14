4392bb24dd31e6aff9067d727e4b31c076012cd2
Anbranin <keichelman@gmail.com>
2019-02-14 16:35:01 -0500

    require modules necessary for system testing, allow logging in with rack session access, and remove duplication for config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,10 +5,22 @@ require 'factory_bot_rails'
 require 'spec_helper'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rspec/rails'
+require 'capybara/rails'
+require 'capybara/rspec'
+require 'rack_session_access/capybara'
 
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.before :all do
+    FactoryBot.reload
+  end
+  config.include FactoryBot::Syntax::Methods
+  config.include UmtsCustomMatchers
   config.infer_spec_type_from_file_location!
   config.use_transactional_fixtures = true
+end
+
+def login_as(user)
+  page.set_rack_session user_id: user.id
 end
