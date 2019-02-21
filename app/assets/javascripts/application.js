3d6811b419e036460e379e99dee3fa21af172f91
Anbranin <keichelman@gmail.com>
2019-02-21 11:06:35 -0500

    remove jquery from gemfile, replace with JS library.
    jquery-ujs is replaced by rails-ujs as of rails 5.1.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require jquery-ui
+//= require rails-ujs
+//= require jquery/dist/jquery.min.js
+//= require jquery-ui/ui/core.js
 //= require moment
 //= require jquery-datetimepicker/jquery.datetimepicker.js
 //= require_tree .
