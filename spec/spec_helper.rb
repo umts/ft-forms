51d7a9394fe6844ebe0d483b4ea470adb6f0408e
Matt Moretti <werebus@gmail.com>
2021-03-09 13:45:27 -0500

    Rewrite methods

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,28 +8,32 @@ SimpleCov.start 'rails' do
   refuse_coverage_drop
 end
 
-# Sets current user based on two acceptable values:
-# 1. a symbol name of a user factory trait;
-# 2. a specific instance of User.
+# Sets current user based on acceptable values:
+# 1. a symbol name of a user factory trait
+# 2. a specific instance of User
+# 3. nil
 def when_current_user_is(user, options = {})
   current_user =
     case user
-    when Symbol
-      create :user, user
-    when User
-      user
-    when nil
-      # need spire for requests but current_user should still be nil
-      session[:spire] = build(:user).spire
-      nil
+    when Symbol then create(:user, user)
+    when User, nil then user
     else raise ArgumentError, 'Invalid user type'
     end
-  if options.key? :view
-    assign :current_user, current_user
-  else session[:user_id] = current_user.try :id
+  set_current_user(current_user)
+end
+
+def set_current_user(user)
+  case self.class.metadata[:type]
+  when :view
+    assign :current_user, user
+  when :feature, :system
+    page.set_rack_session user_id: user.try(:id)
+  when :controller
+    session[:user_id] = user.try(:id)
+    session[:spire] = user.try(:spire) || build(:user).spire
   end
 end
 
 def login_as(user)
-  page.set_rack_session user_id: user.id
+  set_current_user user
 end
