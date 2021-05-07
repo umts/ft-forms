b79e3cc524fae6276a71a236f11ba33b0ee1c430
Matt Moretti <werebus@gmail.com>
2021-05-07 15:41:55 -0400

    Rubocopped spec/system/

--- spec/system/create_form_draft_spec.rb
+++ spec/system/create_form_draft_spec.rb
@@ -7,53 +7,85 @@ RSpec.describe 'creating a draft' do
     when_current_user_is :staff
     visit '/forms'
     click_link 'New form'
-    fill_in 'form_draft[name]', with: 'General Fletcher'
-    fill_in 'form_draft[email]', with: 'fletcher@example.com'
+    fill_in 'Form title', with: 'General Fletcher'
+    fill_in 'Form email destination', with: 'fletcher@example.com'
     fill_in 'form_draft[fields_attributes][0][prompt]', with: 'When?'
     select 'date', from: 'form_draft[fields_attributes][0][data_type]'
   end
 
-  context 'saving a draft' do
-    before do
-      click_button 'Save draft and preview changes'
+  context 'with the draft saved' do
+    before { click_button 'Save draft and preview changes' }
+
+    it 'has the questions' do
       expect(page).to have_text 'When?'
     end
 
     context 'without publishing the form' do
-      it 'displays it on the index page for later' do
-        click_link 'Save draft & go back to index'
-        expect(page).to have_css 'h1', text: 'Unpublished Drafts'
-        expect(page).to have_text 'General Fletcher'
+      before { click_link 'Save draft & go back to index' }
+
+      it 'keeps the form draft' do
         expect(FormDraft.count).to be 1
+      end
+
+      it 'shows that there are unpublished drafts' do
+        expect(page).to have_css 'h1', text: 'Unpublished Drafts'
+      end
+
+      it 'displays the draft on the index page for later' do
+        expect(page).to have_text 'General Fletcher'
       end
     end
 
-    context 'continuing to edit' do
+    context 'when continuing to edit the draft' do
+      before { click_link 'Continue editing' }
+
       it 'brings you back to the edit page for that draft' do
-        click_link 'Continue editing'
         expect(page).to have_current_path edit_form_draft_path(FormDraft.last)
       end
     end
 
-    context 'publishing the form' do
-      it 'discards the draft and creates a form' do
-        click_button 'Publish form and discard draft'
-        expect(page).to have_css 'h1', text: 'Forms'
-        expect(page).to have_text 'General Fletcher'
-        expect(FormDraft.count).to be 0
-        expect(Form.count).to be 1
+    context 'when publishing the form' do
+      before { click_button 'Publish form and discard draft' }
+
+      it 'redirects to the index' do
         expect(page).to have_current_path forms_path
+      end
+
+      it 'discards the draft' do
+        expect(FormDraft.count).to be 0
+      end
+
+      it 'creates a form' do
+        expect(Form.count).to be 1
+      end
+
+      it 'shows that there are forms' do
+        expect(page).to have_css 'h1', text: 'Forms'
+      end
+
+      it 'displays the form' do
+        expect(page).to have_text 'General Fletcher'
       end
     end
   end
 
-  context 'deciding not to create a form or a draft' do
-    it 'deletes the draft' do
-      click_button 'Cancel'
-      expect(page).not_to have_css 'h1', text: 'Unpublished Drafts'
-      expect(page).not_to have_text 'General Fletcher'
-      expect(FormDraft.count).to be 0
+  context 'when deciding not to create a form or a draft' do
+    before { click_button 'Cancel' }
+
+    it 'redirects to the index' do
       expect(page).to have_current_path forms_path
+    end
+
+    it 'deletes the draft' do
+      expect(FormDraft.count).to be 0
+    end
+
+    it 'does not show that there are any drafts' do
+      expect(page).not_to have_css 'h1', text: 'Unpublished Drafts'
+    end
+
+    it 'does not show the draft' do
+      expect(page).not_to have_text 'General Fletcher'
     end
   end
 end
--- spec/system/forms_index_spec.rb
+++ spec/system/forms_index_spec.rb
@@ -3,16 +3,19 @@
 require 'rails_helper'
 
 RSpec.describe 'interacting with forms/drafts from the index' do
-  context 'not staff' do
-    it 'does not allow access' do
+  context 'when the user is not staff' do
+    before do
       when_current_user_is :not_staff
       visit '/forms'
+    end
+
+    it 'does not allow access' do
       warning = 'You do not have permission to access this page'
       expect(page).to have_css 'h1', text: warning
     end
   end
 
-  context 'staff' do
+  context 'when the user is staff' do
     let(:user) { create :user, :staff }
     let!(:form) { create :form, :with_fields }
     let!(:draft) { create :form_draft, user: user }
@@ -22,55 +25,70 @@ RSpec.describe 'interacting with forms/drafts from the index' do
       visit '/forms'
     end
 
-    context 'clicking the delete button', js: true do
-      it 'destroys drafts' do
-        expect(page).to have_text draft.name
-        within('#form-drafts-table') do
-          accept_alert do
-            click_link 'Delete'
-          end
+    context 'when clicking a delete button', js: true do
+      def click_delete_in(selector)
+        within selector do
+          accept_alert { click_link 'Delete' }
         end
-        expect(page).not_to have_text draft.name
+        find('body')
+      end
+
+      it 'destroys drafts' do
+        click_delete_in '#form-drafts-table'
         expect(FormDraft.count).to be 0
+      end
+
+      it 'does not display destroyed drafts' do
+        click_delete_in '#form-drafts-table'
+        expect(page).not_to have_text draft.name
+      end
+
+      it 'informs you of the draft deletion' do
+        click_delete_in '#form-drafts-table'
         within('#flash') do
           expect(page).to have_text 'Draft has been discarded'
         end
       end
 
       it 'destroys forms' do
-        expect(page).to have_text form.name
-        within('#forms-table') do
-          accept_alert do
-            click_link 'Delete'
-          end
-        end
-        expect(page).not_to have_text form.name
+        click_delete_in '#forms-table'
         expect(Form.count).to be 0
+      end
+
+      it 'does not display destroyed forms' do
+        click_delete_in '#forms-table'
+        expect(page).not_to have_text form.name
+      end
+
+      it 'informs you of the form deletion' do
+        click_delete_in '#forms-table'
         within('#flash') do
           expect(page).to have_text 'Form successfully deleted'
         end
       end
     end
 
-    context 'clicking the edit button in the forms table' do
+    context 'when clicking the edit button in the forms table' do
       before do
-        within('#forms-table') do
-          click_link 'Edit'
-        end
+        within('#forms-table') { click_link 'Edit' }
       end
 
       it 'creates a draft for the form and goes to the draft edit page' do
         draft = form.draft_belonging_to(user)
         expect(page).to have_current_path edit_form_draft_path(draft)
-        expect(page).to have_css 'h1', text: "Editing #{form.name}"
       end
 
-      it 'has options in the index to resume editing or discard the draft' do
+      it 'then has the option in the index to resume editing the draft' do
+        draft = form.draft_belonging_to(user)
         click_button 'Save draft and preview changes'
         click_link 'Save draft & go back to index'
-        draft = form.draft_belonging_to(user)
         expect(page).to have_link 'Resume editing saved draft',
                                   href: edit_form_draft_path(draft)
+      end
+
+      it 'then has the option in the index to discard the draft' do
+        click_button 'Save draft and preview changes'
+        click_link 'Save draft & go back to index'
         expect(page).to have_button('Discard saved draft')
       end
     end
